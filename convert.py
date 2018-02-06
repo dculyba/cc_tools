2a9b0ac4c15a9b9e84c3a5fdf1053d29e34f051e
Dave Culyba <dculyba@andrew.cmu.edu>
2018-02-06 01:00:50 -0500

    Added convert.py Removed command line code from test_json_utils

---
+++ convert.py
@@ -0,0 +1,4 @@
+import cc_dat_utils
+
+cc_data = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
+print(cc_data)
--- test_json_utils.py
+++ test_json_utils.py
@@ -1,9 +1,8 @@
 import test_data
-import sys
 import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
-def make_game_library_from_json(json_data):
+def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
@@ -17,20 +16,12 @@ def make_game_library_from_json(json_data):
 
     return game_library
 
-# Handling command line arguments
-#  Note: sys.argv is a list of strings that contains each command line argument
-#        The first element in the list is always the name of the python file being run
-# Command line format: <input json filename>
 
-default_input_json_file = "data/test_data.json"
+input_json_file = "data/test_data.json"
 
-if len(sys.argv) == 2:
-    input_json_file = sys.argv[1]
-    print("Using command line args:", input_json_file)
-else:
-    print("Unknown command line options. Using default values:", default_input_json_file)
-    input_json_file = default_input_json_file
-
-#Load the json data from the input file
+### Begin Add Code Here ###
+#Open the file specified by input_json_file
+#Use the json module to load the data from the file
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
 #Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
+### End Add Code Here ###
